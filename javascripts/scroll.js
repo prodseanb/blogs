da0a7a8c18a7988f6a99d3ddfc9b13273b5158b4
Sean <seanbachiller@gmail.com>
2021-06-05 03:08:35 -0400

    research

--- javascripts/scroll.js
+++ javascripts/scroll.js
@@ -1,42 +1,24 @@
-// Vanilla JavaScript Scroll to Anchor
-// @ https://perishablepress.com/vanilla-javascript-scroll-anchor/
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-(function() {
-	scrollTo();
-})();
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-function scrollTo() {
-	var links = document.getElementsByTagName('a');
-	for (var i = 0; i < links.length; i++) {
-		var link = links[i];
-		if ((link.href && link.href.indexOf('#') != -1) && ((link.pathname == location.pathname) || ('/' + link.pathname == location.pathname)) && (link.search == location.search)) {
-			link.onclick = scrollAnchors;
-		}
-	}
-}
+      // Store hash
+      var hash = this.hash;
 
-function scrollAnchors(e, respond = null) {
-	const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
-	e.preventDefault();
-	var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
-	const targetAnchor = document.querySelector(targetID);
-	if (!targetAnchor) return;
-	const originalTop = distanceToTop(targetAnchor);
-	window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
-	const checkIfDone = setInterval(function() {
-		const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
-		if (distanceToTop(targetAnchor) === 0 || atBottom) {
-			targetAnchor.tabIndex = '-1';
-			targetAnchor.focus();
-			window.history.pushState('', '', targetID);
-			clearInterval(checkIfDone);
-		}
-	}, 100);
-}
-
-document.addEventListener('click', function(e) {
-	event = e || window.e;
-    var target = e.target || e.srcElement;
-    var respond = document.getElementById('respond-link');
-	if (e.target.classList.contains('scroll-live')) scrollAnchors(e, respond);
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });
